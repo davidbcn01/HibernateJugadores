a7f2c2e14b20f34ee2d06d80510b933835215e1c
David Ferrero <dferre@elpuig.xeill.net>
2021-04-19 17:42:39 +0200

    mucho dolor

---
+++ src/main/java/Menu/Menus.java
@@ -0,0 +1,30 @@
+package Menu;
+
+public class Menus {
+    public void MenuPrincipal(){
+        System.out.println("--------------------------------");
+        System.out.println("-------------FUTINFO------------");
+        System.out.println("1.Mostrar jugadores");
+        System.out.println("2.Buscar jugadores de un equipo");
+        System.out.println("3.Buscar jugadores que tengan 1 tipo de carta");
+        System.out.println("4.Añadir un jugador");
+        System.out.println("5.Modificar un jugador");
+        System.out.println("6.Borrar un jugador");
+        System.out.println("7.Mostrar estadisticas");
+        System.out.println("8.Buscar estadisticas de un jugador");
+        System.out.println("9.Añadir estadisticas a un jugador");
+        System.out.println("10.Modificar estadisticas de un jugador");
+        System.out.println("11.Borrar estadisticas de un jugador");
+        System.out.println("12.Mostrar equipos");
+        System.out.println("13.Buscar un equipo");
+        System.out.println("14.Añadir un equipo");
+        System.out.println("15.Modificar un equipo");
+        System.out.println("16.Borrar un equipo");
+        System.out.println("17.Mostrar cartas");
+        System.out.println("18.Buscar una carta");
+        System.out.println("19.Añadir una carta");
+        System.out.println("20.Modificar una carta");
+        System.out.println("21.Borrar una carta");
+
+    }
+}
